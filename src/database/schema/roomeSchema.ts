8c3c65df3d24fcc70ff01baa7286ffc14b893b21
darelljay <darelljason1@gmail.com>
2024-04-13 17:25:04 +0900

    Feat: "Q&A 방 스키마 설계 완료"

---
+++ src/database/schema/roomeSchema.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const roomSchema = new mongoose.Schema({
+    roomName:{
+        type:String,
+        required: true
+    },
+    keycode:{
+        type:String,
+        required:true
+    },
+    parcitipants:[mongoose.Schema.Types.ObjectId],
+
+
+},{timestamps:true});
+
+
+export const Room = mongoose.model('Room',roomSchema);
