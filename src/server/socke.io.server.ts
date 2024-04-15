5a95684bab9c2c83c6b45e9adfe51231e0293a62
darelljay <darelljason1@gmail.com>
2024-04-16 06:19:10 +0900

    Feat: "socket io 서버 구현 완료"

---
+++ src/server/socke.io.server.ts
@@ -0,0 +1,17 @@
+import { Server as HttpServer } from "http";
+import { initChat } from "../socket/controller/chat.controller";
+import { Server } from "socket.io";
+
+export const OpenSocketServer = (server:HttpServer): void =>{
+    server.listen(3001,()=>{
+        console.log('Socket IO server listening on port',3001);
+    });
+
+    const io = new Server(server,{
+        cors:{
+            origin: "*",
+        }
+    });
+
+    initChat(io);
+}
